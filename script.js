60793e41610acf8da64fbde149289689822ec6d7
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-18 18:30:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function myFunction() {
    var value = document.getElementById('myTextarea').value;
-   document.getElementById('output').innerHTML = value + "";
+   document.getElementById('output').innerHTML = 'value';
 }// hello

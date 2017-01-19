0c86e2381fb78ca63d55fc1ad922eb277f5abeb9
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-19 08:41:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function print() {
+function print()
+{
    var deckInText = document.getElementById('deckIn');
    deckOut.value = deckInText.value;
+}
+
+function parse()
+{
+   var lines = $('textarea').val().split('\n');
+   for(var i = 0;i < lines.length;i++){
+    sometext.value += lines[i] + " ";
+   }
 }

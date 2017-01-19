66fc958088a941a4e1643ca4e0c2fa76c711362f
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-19 08:55:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function parse()
 {
    var lines = $('textarea').val().split('\n');
    for(var i = 0;i < lines.length;i++){
-    sometext.value += lines[i] + " ";
+    sometext = "";
+    sometext += lines[i] + " ";
    }
 }

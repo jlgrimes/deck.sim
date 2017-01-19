448839b25e43557e671b88e14220fcaf835332d5
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-19 16:05:38 -0500

    wowzers, i can parse!

--- resources/setup.js
+++ resources/setup.js
@@ -1,18 +1,40 @@
-function print()
-{
-   var deckInText = document.getElementById('deckIn');
-   deckOut.value = deckInText.value;
-}
-
 function parse()
 {  
+   var lines = document.getElementById('deckIn').value.split('\n');
+
+   var target = "";
+   var badLine = false;
    
-   var sometextClone = document.getElementById('deckIn').value;
-   /*
-   var lines = document.getElementById('sometext').innerHTML.split('\n');
    for(var i = 0;i < lines.length;i++){
-      sometextClone += lines[i] + " ";
+
+   	  if (lines[i].includes("on -"))
+   	  {
+      		target = "pokemon";
+      		badLine = true;
+   	  }
+   	  else if (lines[i].includes("Trainer Cards -"))
+   	  {
+   	  		target = "trainers"
+   	  		badLine = true;
+   	  }
+   	  else if (lines[i].includes("Energy -"))
+   	  {
+   	  		target = "energy"
+   	  		badLine = true;
+   	  }
+   	  else
+   	  	badLine = false;
+
+      if (lines[i][0] != '*' && lines[i] != "" && !badLine)
+      {
+      	if (target == "pokemon")
+      		pokemon.push(lines[i]);
+      	else if (target == "trainers")
+      		trainers.push(lines[i]);
+      	else if (target == "energy")
+      		energy.push(lines[i]);
+	  }
    }
-   */
-   document.getElementById("sometext").innerHTML = sometextClone;
+   
+   document.getElementById("sometext").innerHTML = pokemon;
 }

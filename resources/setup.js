9134caec0c8612bbd087d1a2798f5d326f2cd9dd
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-20 11:20:01 -0500

    huge updates

--- resources/setup.js
+++ resources/setup.js
@@ -1,28 +1,11 @@
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
 function parse()
 {  
    var lines = document.getElementById('deckIn').value.split('\n');
 
    var target = "";
    var badLine = false;
+
+   var totalCount = 0;
    
    for(var i = 0;i < lines.length;i++){
 
@@ -50,23 +33,24 @@ function parse()
 
       if (lines[i][0] != '*' && lines[i] != "" && !badLine)
       {
-      	var num = lines[i][0];
+      	var num = lines[i].substr(0, lines[i].indexOf(' '));
       	var j;
       	lines[i] = lines[i].slice(2);
 
       	for (j = 0; j < num; j++)
       	{
-      		deck.push(lines[i]);
-
       		if (target == "pokemon")
-      			pokemon.push(lines[i]);
+      			var tempCard = new card(lines[i], "pokemon");
       		else if (target == "trainers")
-      			trainers.push(lines[i]);
+      			var tempCard = new card(lines[i], "trainer");
       		else if (target == "energy")
-      			energy.push(lines[i]);
+      			var tempCard = new card(lines[i], "energy");
+
+          totalCount++;
+          deck.push(tempCard);
       	}
 	  }
    }
    deck = shuffle(deck);
-   document.getElementById("sometext").innerHTML = deck;
+   document.getElementById("sometext").innerHTML = deck[i].recallName() + " " + deck[i].recallType();
 }

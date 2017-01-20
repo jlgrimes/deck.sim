5793c95c40fa7dc2ccdd28b0c5cab785ef41aec9
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-20 11:22:47 -0500

    added classes and stuff

--- values/deck.js
+++ values/deck.js
@@ -1,4 +1,15 @@
+function card (name, type) {
+	this.name = name;
+	this.type = type;
+}
+
+card.prototype.recallName = function(){
+	return this.name;
+}
+
+card.prototype.recallType = function(){
+	return this.type;
+}
+
 var deck = [];
-var pokemon = [];
-var trainers = [];
-var energy = [];
+made it 

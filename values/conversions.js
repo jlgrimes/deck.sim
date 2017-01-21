06115eb953c2e1e0a4c129cd3da00835c67943a8
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-20 22:40:33 -0500

    just a beta for conversions

---
+++ values/conversions.js
@@ -0,0 +1,34 @@
+function set(abbr){
+	if (abbr == "EVO")
+		return "xy12";
+	else if (abbr == "STS")
+		return "xy11";
+	else if (abbr == "FCO")
+		return "xy10";
+	else if (abbr == "GEN")
+		return "g1";
+	else if (abbr == "BKP")
+		return "xy9"
+	else if (abbr == "BKT")
+		return "xy8"
+	else if (abbr == "AOR")
+		return "xy7"
+	else if (abbr == "ROS")
+		return "dc1"
+	else if (abbr == "DCR")
+		return "xy5"
+	else if (abbr == "PRC")
+		return "xy5"
+	else if (abbr == "PHF")
+		return "xy4"
+	else if (abbr == "FFI")
+		return "xy3"
+	else if (abbr == "FLF")
+		return "xy2"
+	else if (abbr == "XY")
+		return "xy1"
+	else if (abbr == "KSS")
+		return "xy0"
+	else if (abbr == "PR")
+		return "xyp"
+}

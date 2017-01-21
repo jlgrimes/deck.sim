5c5bfb28672762e771054addc3486429563a5a0b
Jared Grimes <jaredlancegrimes@gmail.com>
2017-01-20 22:39:53 -0500

    started making new variables

--- values/deck.js
+++ values/deck.js
@@ -1,6 +1,32 @@
 function card (name, type) {
 	this.name = name;
 	this.type = type;
+
+	var action = name.replace(" ", "_");
+}
+
+function pokemonCard (name, type) {
+	this.name = name;
+	this.hp = hp;
+	this.stage = stage;
+	this.evolvesFrom = evolvesFrom;
+	this.type = type;
+	this.retreat = retreat;
+	this.weakness = weakness;
+	this.resistance = resistance;
+	this.set = set;
+	this.setNum = setNum;
+}
+
+function trainerCard (name, text) {
+	this.name = name;
+	this.action = text;
+}
+
+function energyCard (name, type) {
+	this.name = name;
+	this.cost = cost; // 1 fire, 1 water, and 1 grass would be represented as RWG, a DCE would be CC
+					  // Rainbow energies are represented as A for any
 }
 
 card.prototype.recallName = function(){
@@ -13,3 +39,6 @@ card.prototype.recallType = function(){
 
 var deck = [];
 var hand = [];
+var prizes = [];
+var active;
+var benched = [];

f9f0aa83094942ed7a0b2a2dac6e435bf4feca49
Jared Grimes <jaredlancegrimes@gmail.com>
2017-02-18 17:46:37 -0500

    Added more rulings!

--- src/ruling.js
+++ src/ruling.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(".rule").hide();
     $(".subset").hide();
     $(".set").hide();
+
     $("li").click(function () {
         $(this).next("ul").toggle();
     })

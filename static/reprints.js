f766c176cfe1b0c904191429a4f5a6c5991895f4
Jared Grimes <jaredlancegrimes@gmail.com>
2017-03-03 14:40:45 -0500

    Finished legality!

--- static/reprints.js
+++ static/reprints.js
@@ -133,6 +133,708 @@ var data = [
         number: "122/130",
         reference: "No"
     },
+    {
+        name : "Rainbow Energy",
+        set: "Team Rocket",
+        number: "17",
+        reference: "Yes"
+    },
+    {
+        name : "Rainbow Energy",
+        set: "Team Rocket",
+        number: "80",
+        reference: "Yes"
+    },
+    {
+        name : "Metal Energy",
+        set: "Neo Genesis",
+        number: "19/111",
+        reference: "Yes - EX Ruby & Sapphire or later"
+    },
+    {
+        name : "Super Scoop Up",
+        set: "Neo Genesis",
+        number: "98/111",
+        reference: "No"
+    },
+    {
+        name : "Super Rod",
+        set: "Neo Genesis",
+        number: "103/111",
+        reference: "Yes"
+    },
+    {
+        name : "Darkness Energy",
+        set: "Neo Genesis",
+        number: "104/111",
+        reference: "Yes - EX Ruby & Sapphire or later"
+    },
+    {
+        name : "Energy Retrieval",
+        set: "Legendary Collection",
+        number: "107/110",
+        reference: "Yes"
+    },
+    {
+        name : "Bill",
+        set: "Legendary Collection",
+        number: "108/110",
+        reference: "Yes"
+    },
+    {
+        name : "Potion",
+        set: "Legendary Collection",
+        number: "110/110",
+        reference: "No"
+    },
+    {
+        name : "Copycat",
+        set: "Expedition",
+        number: "138/165",
+        reference: "No"
+    },
+    {
+        name : "Dual Ball",
+        set: "Expedition",
+        number: "139/165",
+        reference: "No"
+    },
+    {
+        name : "Pokemon Reversal",
+        set: "Expedition",
+        number: "146/165",
+        reference: "No"
+    },
+    {
+        name : "Professor Elm's Training Method",
+        set: "Expedition",
+        number: "148/165",
+        reference: "No"
+    },
+    {
+        name : "Super Scoop Up",
+        set: "Expedition",
+        number: "151/165",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "Expedition",
+        number: "153/165",
+        reference: "No"
+    },
+    {
+        name : "Full Heal",
+        set: "Expedition",
+        number: "154/165",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "Expedition",
+        number: "156/165",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "Expedition",
+        number: "157/165",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy (Special)",
+        set: "Expedition",
+        number: "158/165",
+        reference: "Yes"
+    },
+    {
+        name : "Metal Energy (Special)",
+        set: "Expedition",
+        number: "159/165",
+        reference: "Yes"
+    },
+    {
+        name : "Energy Switch",
+        set: "Aquapolis",
+        number: "120/147",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "Aquapolis",
+        number: "142/147",
+        reference: "Yes - EX Ruby & Sapphire or later"
+    },
+    {
+        name : "Metal Energy",
+        set: "Aquapolis",
+        number: "143/147",
+        reference: "Yes - EX Ruby & Sapphire or later"
+    },
+    {
+        name : "Rainbow Energy",
+        set: "Aquapolis",
+        number: "144/147",
+        reference: "No"
+    },
+    {
+        name : "Fisherman",
+        set: "Skyridge",
+        number: "125/144",
+        reference: "No"
+    },
+    {
+        name : "Energy Switch",
+        set: "EX Ruby & Sapphire",
+        number: "82/109",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "EX Ruby & Sapphire",
+        number: "86/109",
+        reference: "No"
+    },
+    {
+        name : "Pokemon Reversal",
+        set: "EX Ruby & Sapphire",
+        number: "87/109",
+        reference: "Yes"
+    },
+    {
+        name : "Energy Search",
+        set: "EX Ruby & Sapphire",
+        number: "90/109",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX Ruby & Sapphire",
+        number: "91/109",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "EX Ruby & Sapphire",
+        number: "93/109",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "EX Ruby & Sapphire",
+        number: "93/109",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy",
+        set: "EX Ruby & Sapphire",
+        number: "94/109",
+        reference: "No"
+    },
+    {
+        name : "Rare Candy",
+        set: "EX Sandstorm",
+        number: "88/100",
+        reference: "Yes"
+    },
+    {
+        name : "Dual Ball",
+        set: "EX Team Magma vs. Team Aqua",
+        number: "72/95",
+        reference: "No"
+    },
+    {
+        name : "Life Herb",
+        set: "EX Hidden Legends",
+        number: "90",
+        reference: "No"
+    },
+    {
+        name : "Energy Switch",
+        set: "EX FireRed & LeafGreen",
+        number: "90/112",
+        reference: "No"
+    },
+    {
+        name : "Great Ball",
+        set: "EX FireRed & LeafGreen",
+        number: "92/112",
+        reference: "No"
+    },
+    {
+        name : "Life Herb",
+        set: "EX FireRed & LeafGreen",
+        number: "93/112",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "EX FireRed & LeafGreen",
+        number: "95/112",
+        reference: "No"
+    },
+    {
+        name : "Pokemon Reversal",
+        set: "EX FireRed & LeafGreen",
+        number: "97/112",
+        reference: "No"
+    },
+    {
+        name : "Super Scoop Up",
+        set: "EX FireRed & LeafGreen",
+        number: "99/112",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX FireRed & LeafGreen",
+        number: "101/112",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "EX FireRed & LeafGreen",
+        number: "102/112",
+        reference: "No"
+    },
+    {
+        name : "Rare Candy",
+        set: "EX Emerald",
+        number: "93/106",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "EX Emerald",
+        number: "86/106",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy",
+        set: "EX Emerald",
+        number: "88/106",
+        reference: "No"
+    },
+    {
+        name : "Energy Switch",
+        set: "EX Unseen Forces",
+        number: "84/115",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "EX Unseen Forces",
+        number: "87/115",
+        reference: "No"
+    },
+    {
+        name : "Pokemon Reversal",
+        set: "EX Unseen Forces",
+        number: "88/115",
+        reference: "No"
+    },
+    {
+        name : "Professor Elm's Training Method",
+        set: "EX Unseen Forces",
+        number: "84/115",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "EX Unseen Forces",
+        number: "94/115",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX Unseen Forces",
+        number: "95/115",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "EX Unseen Forces",
+        number: "96/115",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy",
+        set: "EX Unseen Forces",
+        number: "84/115",
+        reference: "No"
+    },
+    {
+        name : "Dual Ball",
+        set: "EX Delta Species",
+        number: "89/113",
+        reference: "No"
+    },
+    {
+        name : "Great Ball",
+        set: "EX Delta Species",
+        number: "90/113",
+        reference: "No"
+    },
+    {
+        name : "Super Scoop Up",
+        set: "EX Delta Species",
+        number: "100/113",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX Delta Species",
+        number: "101/113",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "EX Delta Species",
+        number: "102/113",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "EX Delta Species",
+        number: "103/113",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy",
+        set: "EX Delta Species",
+        number: "107/113",
+        reference: "No"
+    },
 
+    {
+        name : "Rainbow Energy",
+        set: "EX Legend Maker",
+        number: "81/92",
+        reference: "No"
+    },
 
+    {
+        name : "Rare Candy",
+        set: "EX Holon Phantoms",
+        number: "90/110",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "EX Holon Phantoms",
+        number: "94/110",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy",
+        set: "EX Holon Phantoms",
+        number: "95/110",
+        reference: "No"
+    },
+    {
+        name : "Dual Ball",
+        set: "EX Holon Phantoms",
+        number: "78/100",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "EX Holon Phantoms",
+        number: "82/100",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "EX Holon Phantoms",
+        number: "86/100",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX Holon Phantoms",
+        number: "87/100",
+        reference: "No"
+    },
+    {
+        name : "Copycat",
+        set: "EX Dragon Frontiers",
+        number: "73/101",
+        reference: "No"
+    },
+    {
+        name : "Professor Elm's Training Method",
+        set: "EX Dragon Frontiers",
+        number: "79/101",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "EX Dragon Frontiers",
+        number: "83/101",
+        reference: "No"
+    },
+    {
+        name : "Energy Switch",
+        set: "EX Power Keepers",
+        number: "75/108",
+        reference: "No"
+    },
+    {
+        name : "Great Ball",
+        set: "EX Power Keepers",
+        number: "77/108",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy",
+        set: "EX Power Keepers",
+        number: "87/108",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy",
+        set: "EX Power Keepers",
+        number: "88/108",
+        reference: "No"
+    },
+    {
+        name : "Energy Switch",
+        set: "Diamond & Pearl",
+        number: "107/130",
+        reference: "No"
+    },
+    {
+        name : "PlusPower",
+        set: "Diamond & Pearl",
+        number: "109/130",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "Diamond & Pearl",
+        number: "110/130",
+        reference: "No"
+    },
+    {
+        name : "Super Scoop Up",
+        set: "Diamond & Pearl",
+        number: "115/130",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "Diamond & Pearl",
+        number: "117/130",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "Diamond & Pearl",
+        number: "118/130",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "Diamond & Pearl",
+        number: "119/130",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy (Special)",
+        set: "DP: Mysterious Treasures",
+        number: "119/123",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy (Special)",
+        set: "DP: Mysterious Treasures",
+        number: "120/123",
+        reference: "No"
+    },
+    {
+        name : "PlusPower",
+        set: "DP: Secret Wonders",
+        number: "121/132",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "DP: Secret Wonders",
+        number: "127/132",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "DP: Secret Wonders",
+        number: "128/132",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy (Special)",
+        set: "DP: Secret Wonders",
+        number: "129/132",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy (Special)",
+        set: "DP: Secret Wonders",
+        number: "130/132",
+        reference: "No"
+    },
+    {
+        name : "Rare Candy",
+        set: "DP: Great Encounters",
+        number: "102/106",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "DP: Majestic Dawn",
+        number: "85/100",
+        reference: "No"
+    },
+    {
+        name : "Super Scoop Up",
+        set: "DP: Majestic Dawn",
+        number: "87/100",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "DP: Majestic Dawn",
+        number: "90/100",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy (Special)",
+        set: "DP: Majestic Dawn",
+        number: "93/100",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy (Special)",
+        set: "DP: Majestic Dawn",
+        number: "95/100",
+        reference: "No"
+    },
+    {
+        name : "Energy Switch",
+        set: "DP: Stormfront",
+        number: "84/100",
+        reference: "No"
+    },
+    {
+        name : "Great Ball",
+        set: "DP: Stormfront",
+        number: "85/100",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "DP: Stormfront",
+        number: "92/100",
+        reference: "No"
+    },
+    {
+        name : "Switch",
+        set: "DP: Stormfront",
+        number: "93/100",
+        reference: "No"
+    },
+    {
+        name : "Life Herb",
+        set: "Platinum",
+        number: "108/127",
+        reference: "No"
+    },
+    {
+        name : "PlusPower",
+        set: "Platinum",
+        number: "112/127",
+        reference: "No"
+    },
+    {
+        name : "Poke Ball",
+        set: "Platinum",
+        number: "113/127",
+        reference: "No"
+    },
+    {
+        name : "Rainbow Energy",
+        set: "Platinum",
+        number: "121/127",
+        reference: "No"
+    },
+    {
+        name : "Darkness Energy (Special)",
+        set: "Platinum",
+        number: "99/111",
+        reference: "No"
+    },
+    {
+        name : "Metal Energy (Special)",
+        set: "Platinum",
+        number: "100/111",
+        reference: "No"
+    },
+    {
+        name : "Rare Candy",
+        set: "POP Series 5",
+        number: "7/17",
+        reference: "No"
+    },
+    {
+        name : "Rare Candy",
+        set: "POP Series 8",
+        number: "10/17",
+        reference: "No"
+    },
+    {
+        name : "Rainbow Energy",
+        set: "Holo Energy Promos",
+        number: "NA",
+        reference: "Yes"
+    },
+    {
+        name : "Energy Search",
+        set: "EX: Trainer Kit 1",
+        number: "9/10-B/R",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX: Trainer Kit 1",
+        number: "8/10-B/R",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "EX: Trainer Kit 2",
+        number: "8/12-R, 9/12-B",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "EX: Trainer Kit 2",
+        number: "9/12 R, 10/12 B",
+        reference: "No"
+    },
+    {
+        name : "Energy Search",
+        set: "DP: Trainer's Kit 3",
+        number: "8/11, 10/12",
+        reference: "No"
+    },
+    {
+        name : "Potion",
+        set: "DP: Trainer's Kit 3",
+        number: "9/11, 11/12",
+        reference: "No"
+    },
+    {
+        name : "Victory Medal",
+        set: "Battle Road",
+        number: "(all dates)",
+        reference: "No"
+    },
 ];
